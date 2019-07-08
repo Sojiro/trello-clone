1fe81bd3d5f0500cdd7ac465c4a89fb3a4e41869
Ashok M A <ashok5angel@gmail.com>
2019-07-08 20:30:40 +0200

    Repo initialized with home-logo

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoardListComponent } from './board-list/board-list.component';
+
+@NgModule({
+  declarations: [AppComponent, BoardListComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
